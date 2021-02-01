6451cb9413a598d51bb09018cff5be5b9d43578c
Jeff Inman <jeffery.inman@hotmail.com>
2021-01-31 18:24:50 -0800

    tested code from quiz

--- apps.js
+++ apps.js
@@ -1,52 +1,17 @@
 console.log('hello user')
 
-
-// ADDING ITEMS TO START AND END OF LIST
-var list = document.getElementsByTagName('ul')[0];              
-// Get the <ul> element
-   
-// ADD NEW ITEM TO END OF LIST
-var newItemLast = document.createElement('li');                    
-// Create element
-var newTextLast = document.createTextNode('cream');                
-// Create text node
-newItemLast.appendChild(newTextLast);                              
-// Add text node to element
-list.appendChild(newItemLast);                                     
-// Add element end of list
-
-// ADD NEW ITEM START OF LIST
-var newItemFirst = document.createElement('li');                   
-// Create element
-var newTextFirst = document.createTextNode('kale');                
-// Create text node
-newItemFirst.appendChild(newTextFirst);                            
-// Add text node to element
-list.insertBefore(newItemFirst, list.firstChild);                  
-// Add element to list
-
-
-
-var listItems = document.querySelectorAll('li');                   
-// All <li> elements
-
-// ADD A CLASS OF COOL TO ALL LIST ITEMS
-var i;                                                             
-// Counter variable
-for (i = 0; i < listItems.length; i++) {                           
-  // Loop through elements
-  listItems[i].className = 'cool';                                 
-  // Change class to cool
+var breakfast = ['banana',
+                'chicken wing',
+                'coffee',
+                'eye of newt',
+                'pancakes',
+                'avocado',
+                'fish biscuit',
+                'pecan',
+                'corn dog'];
+var indexOne = 3;
+var indexTwo = 4;
+var indexThree = 6;
+for (var i = indexOne; i < indexThree; i++) {
+  console.log(breakfast[i]);
 }
-
-// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
-var heading = document.querySelector('h2');                        
-// h2 element
-var headingText = heading.firstChild.nodeValue;                    
-// h2 text
-var totalItems = listItems.length;                                 
-// No. of <li> elements
-var newHeading =  headingText + '<span>' + totalItems + '</span>'; 
-// Content
-heading.innerHTML = newHeading;                                    
-// Update h2 using innerHTML (not textContent) because it contains markup
